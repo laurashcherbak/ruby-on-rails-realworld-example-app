9b6ec8934a7649eddb895a55c5421ecd0a3e7d53
Laura Shcherbak <45738929+laurashcherbak@users.noreply.github.com>
2023-11-24 06:25:03 -0800

    Update prometheus.rb

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,10 +1,1 @@
-require 'prometheus_exporter/middleware'
 
-PrometheusExporter::Instrumentation::Middleware.configure do |config|
-  config.collectors = [
-    PrometheusExporter::Middleware::Collector
-  ]
-  config[:in_cluster] = true
-end
-
-Rails.application.middleware.unshift PrometheusExporter::Middleware
